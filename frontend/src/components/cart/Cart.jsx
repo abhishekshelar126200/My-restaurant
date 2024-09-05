1ddc0e9e70fccfac77338b876c169829979aea2d
Abhishek Balkrishna Shelar <116205134+abhishekshelar126200@users.noreply.github.com>
2024-09-06 03:14:48 +0530

    Update Cart.jsx

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -122,7 +122,7 @@ const Cart = ({oncartReduce}) => {
       'Content-Type':'application/json'
     }
 
-    const response=await fetch('http://localhost:4000/create-checkout-session',{
+    const response=await fetch('https://my-restaurant-i131.onrender.com/create-checkout-session',{
       method:"POST",
       headers:headers,
       body:JSON.stringify(item)
